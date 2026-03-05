207c3ff55cfa6e537a417a472f6c1a36c1686fa9
DanilBaril <dapcoelho2708@gmail.com>
2026-03-05 16:24:53 +0000

    add classe PhysicalBook

--- src/main/kotlin/dam/virtual_library/PhysicalBook.kt
+++ src/main/kotlin/dam/virtual_library/PhysicalBook.kt
@@ -1,1 +1,10 @@
 package org.example.dam.virtual_library
+
+class PhysicalBook(
+    title: String,
+    author: String,
+    publicationYear: Int,
+    availableCopies: Int,
+    val weight: Int,
+    val hasHardcover: Boolean = true
+) : Book(title, author, publicationYear, availableCopies) {}
