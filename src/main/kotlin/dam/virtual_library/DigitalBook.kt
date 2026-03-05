cf1ed6a734b05395e6a91ed8099a27bda06832d5
DanilBaril <dapcoelho2708@gmail.com>
2026-03-05 16:14:32 +0000

    add classe DigitalBook

--- src/main/kotlin/dam/virtual_library/DigitalBook.kt
+++ src/main/kotlin/dam/virtual_library/DigitalBook.kt
@@ -1,2 +1,10 @@
 package org.example.dam.virtual_library
 
+class DigitalBook(
+    title: String,
+    author: String,
+    publicationYear: Int,
+    availableCopies: Int,
+    val fileSize: Double,
+    val format: String
+) : Book(title, author, publicationYear, availableCopies) {}
